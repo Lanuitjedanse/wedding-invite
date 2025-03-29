79460924865880e1b613ea30ff75b7e9a317cf98
lucie.bellenger <73110649+Lanuitjedanse@users.noreply.github.com>
2025-03-29 11:38:21 +0100

    add dashboard

--- app/composables/useParticipants.js
+++ app/composables/useParticipants.js
@@ -1,5 +1,60 @@
 export const useParticipants = () => {
   const participants = useState("participants", () => []);
 
-  return { participants };
+  const getTotalNumberOfParticipants = computed(() => {
+    const query = (participant) => participant;
+    return filterParticipants(participants.value, query);
+  });
+
+  const getTotalNumberVege = computed(() => {
+    const query = (participant) => participant.diet === "vegetarian";
+    return filterParticipants(participants.value, query);
+  });
+
+  const getTotalNumberMeat = computed(() => {
+    const query = (participant) => participant.diet === "meat";
+    return filterParticipants(participants.value, query);
+  });
+
+  const getTotalNumberCeremony = computed(() => {
+    const query = (participant) => participant.steps.includes("ceremony");
+    return filterParticipants(participants.value, query);
+  });
+
+  const getTotalNumberCocktail = computed(() => {
+    const query = (participant) => participant.steps.includes("cocktail");
+    return filterParticipants(participants.value, query);
+  });
+
+  const getTotalNumberDinner = computed(() => {
+    const query = (participant) => participant.steps.includes("dinner");
+    return filterParticipants(participants.value, query);
+  });
+
+  const getTotalNumberParty = computed(() => {
+    const query = (participant) => participant.steps.includes("party");
+    return filterParticipants(participants.value, query);
+  });
+
+  const getTotalNumberBrunch = computed(() => {
+    const query = (participant) => participant.steps.includes("brunch");
+    return filterParticipants(participants.value, query);
+  });
+  const filterParticipants = (participants, query) => {
+    return participants
+      ?.filter((i) => query(i))
+      ?.reduce((sum, i) => sum + (i?.additionalParticipants || 0) + 1, 0);
+  };
+
+  return {
+    participants,
+    getTotalNumberOfParticipants,
+    getTotalNumberMeat,
+    getTotalNumberVege,
+    getTotalNumberCeremony,
+    getTotalNumberCocktail,
+    getTotalNumberDinner,
+    getTotalNumberParty,
+    getTotalNumberBrunch,
+  };
 };
---
+++ app/pages/dashboard.vue
@@ -0,0 +1,78 @@
+<script setup>
+const { invitees } = await $fetch("/api/invitees", {
+  method: "GET",
+});
+
+const {
+  participants,
+  getTotalNumberOfParticipants,
+  getTotalNumberVege,
+  getTotalNumberMeat,
+  getTotalNumberCeremony,
+  getTotalNumberCocktail,
+  getTotalNumberDinner,
+  getTotalNumberParty,
+  getTotalNumberBrunch,
+} = useParticipants();
+
+onMounted(() => {
+  participants.value = invitees;
+});
+
+const allParticipants = computed(() => [
+  { value: getTotalNumberOfParticipants, text: "People joining", icon: "" },
+  {
+    value: getTotalNumberCeremony,
+    text: "joining Ceremony",
+    icon: "fluent-emoji-flat:wedding",
+  },
+  {
+    value: getTotalNumberCocktail,
+    text: "joining Cocktail",
+    icon: "fluent-emoji-flat:bottle-with-popping-cork",
+  },
+  {
+    value: getTotalNumberDinner,
+    text: "joining Dinner",
+    icon: "fluent-emoji-flat:pot-of-food",
+  },
+  {
+    value: getTotalNumberParty,
+    text: "joining Party",
+    icon: "fluent-emoji-flat:confetti-ball",
+  },
+  {
+    value: getTotalNumberBrunch,
+    text: "joining Brunch",
+    icon: "fluent-emoji-flat:baguette-bread",
+  },
+  {
+    value: getTotalNumberVege,
+    text: "Vegetarians",
+    icon: "fluent-emoji-flat:herb",
+  },
+  {
+    value: getTotalNumberMeat,
+    text: "Carnivores",
+    icon: "fluent-emoji-flat:meat-on-bone",
+  },
+]);
+</script>
+
+<template>
+  <UContainer>
+    <div
+      class="grid grid-cols-3 gap-8 mx-auto w-max min-h-screen place-content-center"
+    >
+      <div
+        v-for="participant in allParticipants"
+        :key="`item-${participant.text}`"
+        class="flex flex-col font-bold"
+        :class="participant.styles"
+      >
+        <h3 class="text-6xl">{{ participant.value }}</h3>
+        <p class="text-3xl">{{ participant.text }}</p>
+      </div>
+    </div>
+  </UContainer>
+</template>
